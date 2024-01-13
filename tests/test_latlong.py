8fee7cf1b3019439af507c436b03689a84cb3e29
Paul Eastham <eastham@gmail.com>
2024-01-12 18:26:36 -0800

    tests

---
+++ tests/test_latlong.py
@@ -0,0 +1,73 @@
+"""Test for the "latlongring" condition."""
+
+import logging
+import yaml
+
+from stats import Stats
+from adsbactions import AdsbActions
+import testinfra
+
+YAML_STRING = """
+  config:
+    kmls:  # optional KML files that specify geographic regions.
+      - tests/test3.kml 
+
+  aircraft_lists:
+    alert_aircraft: [ "N12345" ] # optional lists of tail numbers of interest.
+
+  rules:
+    nearby:
+      conditions: 
+        min_alt: 4000        # feet MSL, must be >= to match
+        max_alt: 10000       # feel MSL, must be <= to match
+        aircraft_list: alert_aircraft  # use aircraft_list above
+        latlongring: [20, 40.763537, -119.2122323]
+      actions:
+        callback: t1_cb
+
+    readme_rule:
+      conditions: 
+        min_alt: 4000        # feet MSL, must be >= to match
+        max_alt: 10000       # feel MSL, must be <= to match
+        aircraft_list: alert_aircraft  # use aircraft_list above
+        latlongring: [20, 40.763537, -119.2122323]
+        regions: [ "23 upwind" ]
+      actions:
+        callback: t2_cb
+"""
+t1_cb_ctr = 0
+def t1_cb(flight):
+    global t1_cb_ctr
+    t1_cb_ctr += 1
+
+t2_cb_ctr = 0
+def t2_cb(flight):
+    global t2_cb_ctr
+    t2_cb_ctr += 1
+
+JSON_STRING_NEARBY = '{"now": 1661692178, "alt_baro": 5000, "gscp": 128, "lat": 40.751249, "lon": -119.249305, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_BEYOND_REGION_ONLY = '{"now": 1661692178, "alt_baro": 5000, "gscp": 128, "lat": 40.781148, "lon": -119.2449769, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_DISTANT = '{"now": 1661692178, "alt_baro": 5000, "gscp": 128, "lat": 40.5819728, "lon": -119.6232779, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+
+def test_latlong():
+    Stats.reset()
+
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.info('System started.')
+
+    yaml_data = yaml.safe_load(YAML_STRING)
+    adsb_actions = AdsbActions(yaml_data)
+    adsb_actions.register_callback("t1_cb", t1_cb)
+    adsb_actions.register_callback("t2_cb", t2_cb)
+
+    adsb_actions.loop(JSON_STRING_NEARBY)
+    assert t1_cb_ctr == 1
+    assert t2_cb_ctr == 1
+
+    adsb_actions.loop(JSON_STRING_BEYOND_REGION_ONLY)
+    assert t1_cb_ctr == 2
+    assert t2_cb_ctr == 1
+
+    adsb_actions.loop(JSON_STRING_DISTANT)
+    assert t1_cb_ctr == 2
+    assert t2_cb_ctr == 1
---
+++ tests/test_ui.py
@@ -0,0 +1,95 @@
+"""Test for functionality needed to support a UI"""
+import logging
+import yaml
+
+from stats import Stats
+from adsbactions import AdsbActions
+import testinfra
+
+YAML_STRING = """
+  config:
+    kmls:
+      - tests/test2.kml 
+      - tests/test3.kml 
+
+  rules:
+    ui_update:
+      conditions:
+        regions: [ "Scenic", "Gerlach Corridor", "Empire/Razorback/Pattern", "Other" ]
+      actions:
+        callback: aircraft_update_cb
+
+    ui_remove:
+      conditions:
+        regions: ~
+      actions:
+        callback: aircraft_remove_cb
+
+    prox:
+        conditions:
+            min_alt: 3000
+            max_alt: 10000
+            regions: [ "Scenic", "Gerlach Corridor", "Empire/Razorback/Pattern", "Other" ]
+            proximity: [ 400, .3 ] # alt sep in MSL, lateral sep in nm
+        actions:
+            callback: abe_update_cb
+"""
+
+aircraft_update_ctr = aircraft_remove_ctr = abe_update_ctr = 0
+def aircraft_update_cb(flight):
+    global aircraft_update_ctr
+    aircraft_update_ctr += 1
+
+def aircraft_remove_cb(flight):
+    global aircraft_remove_ctr
+    aircraft_remove_ctr += 1
+
+def abe_update_cb(flight):
+    global abe_update_ctr
+    abe_update_ctr += 1
+
+JSON_STRING_3000 = '{"now": 1661692178, "alt_baro": 3000, "gscp": 128, "lat": 40.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_4000 = '{"now": 1661692178, "alt_baro": 4000, "gscp": 128, "lat": 40.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_5000 = '{"now": 1661692178, "alt_baro": 5000, "gscp": 128, "lat": 40.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_11000 = '{"now": 1661692178, "alt_baro": 11000, "gscp": 128, "lat": 40.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_DISTANT = '{"now": 1661692178, "alt_baro": 4000, "gscp": 128, "lat": 41.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_GROUND = '{"now": 1661692178, "alt_baro": 4000, "gscp": 128, "lat": 40.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_GROUND_PLANE2 = '{"now": 1661692178, "alt_baro": 4000, "gscp": 128, "lat": 40.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N5555"}\n'
+JSON_STRING_ZEROALT = '{"now": 1661692178, "alt_baro": 0, "gscp": 128, "lat": 40.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N12345"}\n'
+JSON_STRING_PLANE3_DELAY = '{"now": 1661692185, "alt_baro": 0, "gscp": 128, "lat": 41.763537, "lon": -119.2122323, "track": 203.4, "hex": "a061d9", "flight": "N123456"}\n'
+
+def test_ui():
+    Stats.reset()
+
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+    logging.info('System started.')
+
+    yaml_data = yaml.safe_load(YAML_STRING)
+
+    with open("tests/20minutes.json", 'rt', encoding="utf-8") as myfile:
+        json_data = myfile.read()
+
+    adsb_actions = AdsbActions(yaml_data)
+    adsb_actions.register_callback("aircraft_update_cb", aircraft_update_cb)
+    adsb_actions.register_callback("aircraft_remove_cb", aircraft_remove_cb)
+    adsb_actions.register_callback("abe_update_cb", abe_update_cb)
+
+    adsb_actions.loop(JSON_STRING_GROUND)
+    assert aircraft_update_ctr == 1
+    adsb_actions.loop(JSON_STRING_ZEROALT)
+    assert aircraft_remove_ctr == 1
+
+    # set up two aircraft in the same position for prox testing
+    adsb_actions.loop(JSON_STRING_GROUND)
+    adsb_actions.loop(JSON_STRING_GROUND_PLANE2)
+    adsb_actions.loop(JSON_STRING_PLANE3_DELAY) # allow time to pass for checkpoint
+    assert abe_update_ctr == 2  # one for each plane near the other
+
+    if True:
+        adsb_actions.loop(json_data)
+
+        # check the number of aircraft left visible after expiry etc
+        rendered_flight_ctr = 0
+        for f in adsb_actions.flights.flight_dict.values():
+            rendered_flight_ctr += 1 if f.in_any_bbox() else 0
+        assert rendered_flight_ctr == 4 
