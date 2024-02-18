493bf4538ec41605c107e030a0498a086f3b2c9a
Paul Eastham <eastham@gmail.com>
2024-02-17 20:48:30 -0800

    cleanup

--- src/adsb_actions/page.py
+++ src/adsb_actions/page.py
@@ -7,6 +7,9 @@ from config import Config
 
 CONFIG = Config()
 
+SEND_SLACK = False
+SEND_PAGE = True
+
 # TODO make this more generic -- the "webhook" action ideally could
 # cover the functionality of either of these methods.
 # maybe change the format of the webhook action to be something like:
@@ -26,10 +29,25 @@ def send_slack(text):
         print("Skipping slack send")
 
 def send_page(recipient: str, msg: str):
-    """Send a page with body of "msg"."""
+    """Send a page with a JSON payload.
+
+    Args:
+        recipient (str): The recipient of the page.
+        msg (str): The message to send.
+
+    Returns:
+        bool: True if the page was sent successfully, False otherwise.
+    """
+    if not SEND_PAGE:
+        print("Skipping page send")
+        return False
 
     # lookup the recipient's id from the config
-    recipient_id = CONFIG.private_vars['page_recipients'][recipient.lower()]
+    try:
+        recipient_id = CONFIG.private_vars['page_recipients'][recipient.lower()]
+    except KeyError:
+        print(f"Recipient {recipient} not found in config")
+        return False
 
     body = {
     "username": CONFIG.private_vars['page_user'],
@@ -43,18 +61,20 @@ def send_page(recipient: str, msg: str):
             msg
     }}
 
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-
     requests_log = logging.getLogger("requests.packages.urllib3")
     requests_log.setLevel(logging.DEBUG)
     requests_log.propagate = True
 
-    response = requests.Session().post(CONFIG.private_vars['page_url'], json=body)
-    print(response)
-    print(response.text)
-    return response.text
+    response = requests.Session().post(CONFIG.private_vars['page_url'],
+                                       json=body)
+    success = "success" in response.json().get('status')
+
+    print("Page success: " + str(success))
+    print("Page response: " + response.text)
+    return success
 
 if __name__ == "__main__":
     # first arg: recipient, remaining args: message
-    send_page(sys.argv[1], " ".join(sys.argv[2:]))
+    ret = send_page(sys.argv[1], " ".join(sys.argv[2:]))
+
+    sys.exit(0 if ret else 1)
