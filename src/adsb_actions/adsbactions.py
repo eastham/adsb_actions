6fe1db9deec962ce3efcd5d26db0e625b02caa90
Harper Mann <hmann@lbl.gov>
2024-03-07 16:54:29 -0800

    - added args for metrics port - added prometheus metrics to AdsbActions

--- src/adsb_actions/adsbactions.py
+++ src/adsb_actions/adsbactions.py
@@ -2,7 +2,7 @@
 
 The following code will instantiate the library, attempt to connect to a network
 socket, and process the ADS-B data coming in:
-    adsb_actions = (yaml_config, ip=args.ipaddr, port=args.port, mport args.mport)
+    adsb_actions = (yaml_config, ip=args.ipaddr, port=args.port, mport=args.mport)
     adsb_actions.loop()
 
 Also useful, to support rules that want to call code:
@@ -26,6 +26,12 @@ from flights import Flights
 from bboxes import Bboxes
 from stats import Stats
 from location import Location
+
+# Experimental Prometheus exporter
+from prometheus_client import start_http_server, Summary, Gauge, Info
+
+# Define some prometheus metrics
+test_gauge = Gauge('adsb_actions_appsheet_error', 'Appsheet errors', [ 'message' ])
 
 logger = logging.getLogger(__name__)
 
@@ -61,6 +67,10 @@ class AdsbActions:
 
         if ip and port:
             self.listen = self._setup_network(ip, port)
+
+        if mport:
+            start_http_server(mport)
+            test_gauge.labels('Set from Adsb_actions').set(42)
 
     def loop(self, string_data = None, iterator_data = None, delay: float = 0.) -> None:
         """Process ADS-B json data in a loop on the previously-opened socket.
