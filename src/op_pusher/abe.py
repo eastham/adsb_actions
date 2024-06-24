dbbb1a4a274d2c68cabac3264e436f5742f205d0
Paul Eastham <eastham@gmail.com>
2024-06-24 16:43:23 -0700

    logger fixes contd

--- src/op_pusher/abe.py
+++ src/op_pusher/abe.py
@@ -2,16 +2,16 @@
 These are pushed once upon first detection and again once
 expired, so that the minimum distance is logged."""
 
+import logging
 import threading
 import time
 
 from db_ops import add_abe, update_abe
 from adsb_actions.stats import Stats
 
-import adsb_logger
-from adsb_logger import Logger
+from adsb_actions.adsb_logger import Logger
 
-logger = adsb_logger.logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 #logger.level = adsb_logger.logging.DEBUG
 LOGGER = Logger()
 
