a871a35d324ef6c0554db7e3f2cfce0b3df96dca
Paul Eastham <eastham@gmail.com>
2024-08-21 16:59:55 -0700

    Hack for exclusions

--- src/op_pusher/op_pusher_helpers.py
+++ src/op_pusher/op_pusher_helpers.py
@@ -9,10 +9,21 @@ from adsb_actions.adsb_logger import Logger
 logger = logging.getLogger(__name__)
 #logger.level = logging.DEBUG
 LOGGER = Logger()
+EXCLUDE_SUBSTRS = ["N10C", "N10D"]  # XXX HACK
 
 Stats.register_prom_callbacks()
 
+def check_exclusions(flight):
+    for substr in EXCLUDE_SUBSTRS:
+        if substr in flight.flight_id:
+            logger.debug("Excluded flight %s", flight.flight_id)
+            return True
+    return False
+
 def landing_cb(flight):
+    if check_exclusions(flight):
+        return
+
     logger.info("Landing detected! %s", flight.flight_id)
     if 'note' in flight.flags:
         logger.info("Local-flight landing detected! %s", flight.flight_id)
@@ -22,11 +33,17 @@ def landing_cb(flight):
     add_op(flight, "Landing", 'note' in flight.flags)
 
 def popup_takeoff_cb(flight):
+    if check_exclusions(flight):
+        return
+
     logger.info("Popup takeoff detected! %s", flight.flight_id)
     Stats.popup_takeoffs += 1
     takeoff_cb(flight)
 
 def takeoff_cb(flight):
+    if check_exclusions(flight):
+        return
+
     logger.info("Takeoff detected! %s", flight.flight_id)
     Stats.takeoffs += 1
 
@@ -34,6 +51,8 @@ def takeoff_cb(flight):
 
 def abe_cb(flight1, flight2):
     """ABE = Ads-B Event -- two airplanes in close proximity"""
+    if check_exclusions(flight1) or check_exclusions(flight2):
+        return
     logger.info("ABE detected! %s", flight1.flight_id)
     process_abe_launch(flight1, flight2)
 
